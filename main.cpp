4c550c004ffaaa5d6c75b32c47d4c5fcc05f7f39
admin <gadzukoleksandr29@gmail.com>
2024-11-30 11:54:29 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <iostream>
+#include <Windows.h>
 
-int main ()
+using namespace std;
+
+int main()
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    double scale,distanc, dist;
+    
+    system("cls");
+    
+    cout << "Payments vary between settlements.\nenter your business details" << endl;
+    cout << "Map scale (number of kilometers in one centimeter: ";
+    SetConsoleTextAttribute(hConsole, 4);
+    cin >> scale;
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "Payments vary between settlements: ";
+    SetConsoleTextAttribute(hConsole, 4);
+    cin >> distanc;
+    SetConsoleTextAttribute(hConsole, 7);
+
+    dist = scale * distanc;
+    
+    cout << "The distance between settlements is: ";
+    SetConsoleTextAttribute(hConsole, 4);
+    cout << dist;
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << " km" << endl;
+    
+    return 0;
 }
