30b1fd1aa7e0fd047c2c84602fcea73f36e1324f
admin <gadzukoleksandr29@gmail.com>
2024-11-30 11:55:01 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <iostream>
+#include <Windows.h>
 
-int main ()
+using namespace std;
+
+int main()
 {
+    double radius,volume,pi;
+    pi = 3.14;
+
+    cout << "Calculation of the volume of the cooler.\n";
+    cout << "radius: ";
+    cin >> radius;
+
+    volume = (4.0 / 3.0) * pi * pow(radius, 3);
+
+    cout << "Coolet volume: " << volume << " kb.cm\n";
+
+    return 0;
 }
