559090befc02ef953bbff220b5da31d21a7b6a12
admin <gadzukoleksandr29@gmail.com>
2024-11-30 11:52:09 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <iostream>
 
-int main ()
+using namespace std;
+
+int main()
 {
+    int grn, kop;
+    cout << "grn: ";
+    cin >> grn;
+    cout << "kop: ";
+    cin >> kop;
+    if (kop>=99)
+    {
+        grn += kop / 100;
+        kop = kop % 100;
+        cout << grn << " grn " << kop << " kop " << endl;
+    }
+    else
+    {
+        cout << grn << " grn " << kop << " kop " << endl;
+    }
+    return 0;
 }
