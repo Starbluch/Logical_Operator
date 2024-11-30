1023bed29e7701844c875e371bd0aae8d28a5ff7
admin <gadzukoleksandr29@gmail.com>
2024-11-30 11:51:21 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <iomanip>
+#include <cmath>
+#include <string>
 
-int main ()
-{
+using namespace std;
+
+int main() {
+    int years;
+
+    cout << "Enter years: ";
+    cin >> years;
+
+    if ( years % 4 == 0 )
+    {
+        cout << "366" << endl;
+    }
+    else
+    {
+        cout << "365" << endl;
+    }
+    return 0;
 }
+
