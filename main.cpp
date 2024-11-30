63e1015c6bfdea4e39e722a3ce4e2b79ad3ac074
admin <gadzukoleksandr29@gmail.com>
2024-11-30 11:56:45 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <iostream>
+#include <Windows.h>
 
-int main ()
+using namespace std;
+
+int main()
 {
+    int god , cek;
+    int cek_const = 28800;
+
+    cout << "cek: ";
+    cin >> cek;
+    if (cek >= cek_const)
+    {
+        cout << "The working day is over!" << endl;
+        return 0;
+    }
+
+    cek = cek_const - cek;
+    god = (cek / 60) / 60;
+    
+    
+    cout << "End of the working day in:" << god << endl;
+    return 0;
 }
